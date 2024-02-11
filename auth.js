81126e88fee4674d0e827866324e1d5021349f9c
Samuel Rinnetmäki <samuel@iki.fi>
2024-02-11 08:35:15 +0200

    use different ssiConfigs for issuer and verifier

--- auth.js
+++ auth.js
@@ -1,15 +1,15 @@
-export async function auth(config) {
+import config from './config.json' assert {'type': 'json'}
+export default async function auth(config, template) {
     const pwStr = [config.api_id, config.api_password].join(':')
     const b64 = Buffer.from(pwStr).toString('base64')
     const params = {
         method: 'POST',
         headers: {
             tenant: config.tenant,
-            ssiconfig: config.template,
+            ssiconfig: template,
             Authorization: `Basic ${b64}`
         }
     }
-    
     // console.log(config.login_url, params)
     const resp = await fetch(config.login_url, params)
     if (resp.status == 409) {
